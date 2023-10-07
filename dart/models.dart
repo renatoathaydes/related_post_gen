1792622fb16fe367776351cab86b04d441bff045
Renato Athaydes <renato@athaydes.com>
2023-10-07 15:36:26 +0200

    Don't use Dart dynamic.

--- dart/models.dart
+++ dart/models.dart
@@ -9,13 +9,20 @@ class Post {
     required this.tags,
   });
 
-  factory Post.fromJson(dynamic json) => Post(
-        iD: json['_id'],
-        title: json['title'],
-        tags: List.from(json['tags']),
-      );
+  factory Post.fromJson(Object? object) {
+    return switch (object) {
+      Map<String, Object?> json => Post(
+          iD: json['_id'] as String,
+          title: json['title'] as String,
+          tags: (json['tags'] as List<dynamic>)
+              .map((e) => e.toString())
+              .toList(growable: false),
+        ),
+      _ => throw Exception('Invalid post: $object'),
+    };
+  }
 
-  Map<String, dynamic> toJson() => {
+  Map<String, Object> toJson() => {
         '_id': iD,
         'title': title,
         'tags': tags,
